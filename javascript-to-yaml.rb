ef5bb234ef5da8cfbc2b6e466416d044dfedbb24
Greg Stanley <gstanley@frii.com>
2016-07-18 17:58:48 -0600

    .

--- javascript-to-yaml.rb
+++ javascript-to-yaml.rb
@@ -1,1 +1,16 @@
 # export yaml for javascript ast
+
+require "rubygems"
+require "rkelly"
+require "yaml"
+
+def export_yaml(ast)
+  ast.to_yaml
+end
+
+def file_to_ast(file)
+  parser = RKelly::Parser.new
+
+  src = File.read(file)
+  parser.parse(src).to_sexp
+end
