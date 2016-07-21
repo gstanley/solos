46fd7897275aebae558234dbdb3dc78d64dbd216
Greg Stanley <gstanley@frii.com>
2016-07-21 17:33:44 -0600

    ast and yaml functions command line

--- ruby-to-yaml.rb
+++ ruby-to-yaml.rb
@@ -1,1 +1,20 @@
 # export yaml for ruby ast
+
+require "ripper"
+require "yaml"
+
+def export_yaml(ast)
+  ast.to_yaml
+end
+
+def file_to_ast(file)
+  src = File.read(file)
+  Ripper.sexp(src)
+end
+
+if __FILE__ == $0
+  ARGV.each do |file|
+    puts file + ":"
+    puts export_yaml(file_to_ast(file))
+  end
+end
