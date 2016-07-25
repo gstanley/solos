42f55da5708baa36f7b04bda5006790d4243783a
Greg Stanley <gstanley@frii.com>
2016-07-25 17:07:34 -0600

    targets for metadata and stubs/undo wrappers/wrappers

---
+++ wrapper-targets.rb
@@ -0,0 +1,20 @@
+# setup wrapper targets
+# global
+$x = 23
+# object/struct
+# random number
+# time/date
+# environment variable
+# command line arguments
+# stdin/stdout/stderr
+# directory
+# file
+# exception throwing expression
+# process
+# system command
+# runtime environment
+
+# sensors
+
+# client code (to be wrapped)
+#   with metadata
