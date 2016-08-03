e3470d9d59ffdd1c6d959db10d17ecfa0ca79161
Greg Stanley <gstanley@frii.com>
2016-08-03 09:40:33 -0600

    switch format to yaml

--- trace-sieve.rb
+++ trace-sieve.rb
@@ -1,16 +1,18 @@
-# sieve of Eratosthenes
-source: max = Integer(ARGV.shift || 100)
-deps:
-
-sieve = []
-for i in 2 .. max
-  sieve[i] = i
-end
-
-for i in 2 .. Math.sqrt(max)
-  next unless sieve[i]
-  (i*i).step(max, i) do |j|
-    sieve[j] = nil
-  end
-end
-puts sieve.compact.join(", ")
+- def:
+    lang: ruby
+    file: sieve.rb
+- src: # sieve of Eratosthenes
+- src: max = Integer(ARGV.shift || 100)
+  deps:
+- src: sieve = []
+- src: for i in 2 .. max
+- src:   sieve[i] = i
+- src: end
+- src: 
+- src: for i in 2 .. Math.sqrt(max)
+- src:   next unless sieve[i]
+- src:   (i*i).step(max, i) do |j|
+- src:     sieve[j] = nil
+- src:   end
+- src: end
+- src: puts sieve.compact.join(", ")
