f1e375292f2c512747a1ab858798e54e057aa8ad
Greg Stanley <gstanley@frii.com>
2016-08-03 17:41:17 -0600

    stub for show generated source refactor out populate_line_numbers

--- exec-yaml.rb
+++ exec-yaml.rb
@@ -4,6 +4,7 @@ def load_yaml_file(name)
   YAML.load(File.read(name))
 end
 
+# dependent on load_yaml_file
 def merge_defaults(source)
   default = {}
   source.map do |elem|
@@ -16,16 +17,51 @@ def merge_defaults(source)
   end
 end
 
-def show_source(source, line_numbers)
+# dependent on merge_defaults
+def populate_line_numbers(source)
   line_number = 1
+  source.map do |elem|
+    if elem["src"]
+      if elem["line"]
+        line_number = elem["line"]
+      else
+        elem["line"] = line_number
+      end
+      line_number += 1
+    end
+    elem
+  end
+end
+
+# dependent on populate_line_numbers
+def show_source(source, line_numbers)
   source.each do |elem|
     if elem["src"]
       if line_numbers
-        print "#{line_number}: "
-        line_number += 1
+        print "#{elem["line"]}: "
       end
       puts elem["src"]
     end
   end
 end
+
+# dependent on merge_defaults
+def show_gen_source(source)
+  source.each do |elem|
+    if elem["src"]
+      puts elem["src"]
+    end
+  end
+end
+
+#def exec_line(source, line)
+#  line_number = 1
+#  source.each do |elem|
+#    if elem["src"]
+#      if line_numbers
+#        line_number += 1
+#      end
+#    end
+#  end
+#end
 
