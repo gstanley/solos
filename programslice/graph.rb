6a8f80a13c509e97ebb3740b50a8a7239c82245e
Greg Stanley <gstanley@frii.com>
2016-10-05 09:53:32 -0600

    fix connect

--- programslice/graph.rb
+++ programslice/graph.rb
@@ -8,8 +8,21 @@ class Graph
 
   def add(edge)
     # Adds a new edge with the given parameters to the graph.
-    graph.set_default(edge, [])
+    graph[edge] ||= []
     edge
+  end
+
+  def connect(e1, e2)
+    #assert isinstance(e1, type(e1)) and isinstance(e2, type(e2))
+    edges = graph[e1] ||= []
+    if !edges.include?(e2)
+      graph[e1] << e2
+    end
+    add(e2)
+  end
+
+  def [](key)
+    graph[key]
   end
 end
 
