f7ee31b39b8bd15547349fde628eda8b6d6d86ac
Greg Stanley <gstanley@frii.com>
2016-11-03 18:51:18 -0600

    start parsing

--- qd-parsers/parse.rb
+++ qd-parsers/parse.rb
@@ -6,17 +6,24 @@ require "find"
 require "treetop"
 
 ["ruby", "javascript", "java"].each {|name| Treetop.load("./#{name}")}
+RUBY_PARSER = RubyParser.new
+JAVASCRIPT_PARSER = JavascriptParser.new
+JAVA_PARSER = JavaParser.new
 
 def parse_files(path)
   Find.find(path) do |f|
     next if FileTest.directory?(f)
     dir, file = File.split(f)
+    puts f
+    puts "=" * 40
+    contents = File.read(f)
     case File.extname(f)
     when ".rb"
-      puts f
+      puts RUBY_PARSER.parse(contents).text_value
     when ".js"
     when ".java"
     end
+    puts "=" * 40
   end
 end
 
