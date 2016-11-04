0f9ab1ee2cc3f7dab8da9061b68a92694a344775
Greg Stanley <gstanley@frii.com>
2016-11-03 18:31:39 -0600

    iterate over files

--- qd-parsers/parse.rb
+++ qd-parsers/parse.rb
@@ -1,3 +1,22 @@
 # read symbols
 # generate call graph
 # select ruby, javascript, java parser
+
+require "find"
+require "treetop"
+
+["ruby", "javascript", "java"].each {|name| Treetop.load("./#{name}")}
+
+def parse_files(path)
+  Find.find(path) do |f|
+    next if FileTest.directory?(f)
+    dir, file = File.split(f)
+    case File.extname(f)
+    when ".rb"
+      puts f
+    when ".js"
+    when ".java"
+    end
+  end
+end
+
