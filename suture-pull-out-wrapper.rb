cc65f0ea3d12d6bbd35341a5aeccee678174210e
Greg Stanley <gstanley@frii.com>
2017-01-17 15:01:12 -0700

    add normal call

--- suture-pull-out-wrapper.rb
+++ suture-pull-out-wrapper.rb
@@ -19,6 +19,10 @@ class Controller
   def show
 debugger
     calc = Calculator.new
+    # wrapper - before
+    # wrapper - call
+    calc.add(params[:left], params[:right])
+    # suture - call
     @result = Suture.create :add,
       old: calc.method(:add),
       args: [
@@ -26,6 +30,7 @@ debugger
         params[:right]
       ],
       record_calls: true
+    # wrapper - after
   end
 end
 
