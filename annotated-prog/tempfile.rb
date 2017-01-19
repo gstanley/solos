2e1c15e396add86b7085d3840ce0384e26b897c3
Greg Stanley <gstanley@frii.com>
2017-01-18 18:12:40 -0700

    .

--- annotated-prog/tempfile.rb
+++ annotated-prog/tempfile.rb
@@ -1,8 +1,20 @@
 require 'tempfile'
 
+#<
+#  <
+#    <
+#      <
+#        <
+#          <
 f = Tempfile.new("foo")
+#          >
 f.print("foo\n")
+#        >
 f.close
+#      >
 f.open
+#    >
 p f.gets # => "foo\n"
+#  >
 f.close!
+#>
