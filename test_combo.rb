f4073ee158601e9c60ea673d481b89c1f6f873be
Greg Stanley <gregory.stanley@optum.com>
2017-05-18 15:28:14 -0600

    test combos

---
+++ test_combo.rb
@@ -0,0 +1,19 @@
+def test_combo(params, prefix = "")
+  if params.empty?
+    if prefix.empty?
+      []
+    else
+      [prefix]
+    end
+  else
+    result = []
+    first = params.keys.first
+    values = params[first]
+    rest = params.dup
+    rest.delete(first)
+    values.each do |value|
+      result += test_combo(rest, "#{prefix}#{prefix.empty? ? "" : ";"}#{first}=#{value.to_s}")
+    end
+    result
+  end
+end
