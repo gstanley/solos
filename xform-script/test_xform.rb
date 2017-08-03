e7dd6480758c49c525251b7d50fb1874c9341ce2
Greg Stanley <gregory.stanley@optum.com>
2017-08-03 12:24:27 -0600

    new tests

--- xform-script/test_xform.rb
+++ xform-script/test_xform.rb
@@ -1,8 +1,74 @@
 require 'test/unit'
+require 'pry'
 require './xform'
 
 class TestXForm < Test::Unit::TestCase
-  test "xform an empty script results in same" do
-    assert_equal({a: 123}, xform({a: 123}, {}))
+  test "empty script and empty source results in empty" do
+    assert_equal [], xform([], [])
+  end
+
+  test "empty script results in input source" do
+    assert_equal ['a'], xform(['a'], [])
+  end
+
+  test "point match with empty script" do
+    assert_equal ['a'], xform([], ["~/a"])
+  end
+
+  test "match a and replace with itself" do
+    assert_equal ['a'], xform(['a'], ["a/a"])
+  end
+
+  test "fail to match a results in original source" do
+    assert_equal ['b'], xform(['b'], ["a/a"])
+  end
+
+  test "match a and replace the element" do
+    assert_equal ['b'], xform(['a'], ["a/b"])
+  end
+
+  test "match anything and replace the element" do
+    assert_equal ['b'], xform(['a'], ["./b"])
+  end
+
+  test "match a and remove it" do
+    assert_equal [], xform(['a'], ["a/"])
+  end
+
+  test "delete a from empty source" do
+    assert_equal [], xform([], ["a/"])
+  end
+
+  test "add element to list" do
+    assert_equal ['a', 'b'], xform(['a'], ["a/a", "~/b"])
+  end
+
+  test "add element to beginning of list" do
+    assert_equal ['b', 'a'], xform(['a'], ["~/b", "a/a"])
+  end
+
+  test "add element to beginning of list with transfer of excess source" do
+    assert_equal ['b', 'a'], xform(['a'], ["~/b"])
+  end
+
+  test "replace element in middle of list" do
+    assert_equal ['a', 'x', 'c'], xform(['a', 'b', 'c'], ["b/x"])
+  end
+
+  test "insert element in middle of list" do
+    assert_equal ['a', 'x', 'c'], xform(['a', 'c'], ["a/a", "~/x"])
+  end
+
+  test "replace with match" do
+    assert_equal ['a'], xform(['a'], ["./."])
+  end
+
+  test "replace with escaped ." do
+    binding.pry
+    assert_equal ['.'], xform(['a'], ["./\\."])
+  end
+
+  test "replace with escaped \\" do
+    assert_equal ["\\"], xform(['a'], ["./\\"])
   end
 end
--- xform-script/xform.rb
+++ xform-script/xform.rb
@@ -1,3 +1,46 @@
+# ~ - point (taken from emacs) matches in between elements 
 def xform(source, script)
-  source
+  result = []
+  source_enum = source.each
+  script_enum = script.each
+  hold_script_elem = false
+  script_elem = nil
+  loop do
+    unless hold_script_elem
+      script_elem = script_enum.next rescue :done
+    end
+    matcher, value = if script_elem != :done
+                       script_elem.split('/')
+                     else
+                       [nil, nil]
+                     end
+    if matcher && matcher.length > 1 && matcher[0] == "\\"
+      matcher = matcher[1..-1]
+    end
+    if value && value.length > 1 && value[0] == "\\"
+      value = value[1..-1]
+      value_escaped = true
+    else
+      value_escaped = false
+    end
+    if matcher != '~'
+      source_elem = source_enum.next rescue :done
+    end
+    break if source_elem == :done && script_elem == :done
+    hold_script_elem = false
+    if script_elem != :done
+      if matcher == source_elem || matcher == '~' || matcher == '.'
+        if value
+          result << (value == '.' && !value_escaped ? source_elem : value)
+        end
+      elsif source_elem != :done
+        result << source_elem
+        hold_script_elem = true
+      end
+    elsif source_elem != :done
+      result << source_elem
+    end
+  end
+
+  result
 end
